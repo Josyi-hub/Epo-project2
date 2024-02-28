ef01d087973f7b8280ffd81a6268cf4df9d39fa5
Bayala Josias Yipenenye <yipenenye1@gmail.com>
2024-02-28 16:59:45 +0000

    Ajout d'une galerie d'images pour l'EPO

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ document.getElementById('dynamicContent').textContent = 'Le contenu a été chan
 window.onload = function() { 
 changeDynamicContent(); 
 }; 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll("#galerie img");
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      alert("Vous avez cliqué sur une image !");
+    });
+  });
+}); 
